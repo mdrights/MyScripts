ad1e75be6c5b00aa28b3df8aad27389e0665ae62
Ubuntu <ubuntu@ip-172-31-22-211.ap-northeast-1.compute.internal>
2016-06-11 10:08:38 +0000

    update system_info.sh

---
+++ system_info3.0.sh
@@ -0,0 +1,79 @@
+#!/bin/bash
+# Showing system info. 2015-12-23
+# And email them. 2016-03-29
+# Remove colorized tag for email_version. 2016-06-11
+
+touch $HOME/system-info.txt 
+Result="$HOME/system-info.txt"
+
+echo -e "***** HOSTNAME INFORMATION *****" > $Result
+hostnamectl >> $Result
+echo >> $Result
+
+echo -e "***** DISK SPACE USAGE *****" >> $Result
+df -h >> $Result
+echo >> $Result
+
+echo -e "***** FREE AND USED MEMORY *****" >> $Result
+free -h >> $Result
+echo >> $Result
+
+echo -e "***** SYSTEM UPTIME AND LOAD *****" >> $Result
+uptime >> $Result
+echo >> $Result
+
+echo -e "***** CURRENTLY LOGGED-IN USERS *****" >> $Result
+who >> $Result
+echo >> $Result
+
+#echo -e "\e[31;43m***** TOP 5 MEMORY-CONSUMING PROCESSES *****\e[0m" 
+#ps -e o pid,-%mem,%cpu,comm --sort=%mem | head -n 6
+#echo
+
+
+
+#Checking FILE SYSTEM USAGE
+
+echo >> $Result
+echo -e "CHECKING FILE SYSTEM USAGE...." >> $Result
+
+THRESOLD=80
+n=2
+while [ $n -le $(echo "`df`" | wc -l) ]
+do
+	NUM=$n
+	FILESYSTEM=$(echo "`df`" | awk 'NR=='$NUM' {print $1}') 
+	PERCENTAGE=$(echo "`df`" | awk 'NR=='$NUM' {print $5}')
+	USAGE=${PERCENTAGE%?}
+	# echo "$FILESYSTEM; $PERCENTAGE; $USAGE." >> $Result
+	n=$[ $n + 1 ]
+
+if [ $USAGE -gt $THRESOLD ]; then
+	echo >> $Result
+	echo "The remaining available space in $FILESYSTEM is critically low. Used: $PERCENTAGE" >> $Result
+fi
+
+done
+#< <(df -h --total | grep -vi filesystem)
+
+# The open ports and services.
+echo -e "***** OPEN SERVICES *****" >> $Result
+ss -tulp | grep -o "users.*" >> $Result
+
+echo -e "***** ESTABLISHED CONNECTIONS *****" >> $Result
+ss -tu >> $Result
+
+
+
+echo "Sending email..." >> $Result
+
+cat $Result | mutt -s "VPS info at `date`, `hostname`" mdrights@icloud.com
+
+echo
+
+echo -e "Done." >> $Result
+
+echo
+exit 0
+
+
