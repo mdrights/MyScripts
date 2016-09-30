c31d837107cf924e69f6f3d6ff279dcaf774f35a
mdrights <linusyangcn@gmail.com>
2016-09-30 04:30:34 +0100

    update it.

--- convert.sh
+++ convert.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 # 2016.08.13
 
-Dir="/media/linusyeung/SHIT/NewFolder/"
-File="$(ls -1 $Dir)"
-Pattern=".html"
+Pattern="htm"
+Dir="$PWD"
+File=$(find $PWD -name \*.$Pattern)
 
 for file in $File
 do
-	ofile="${file%$Pattern}".md
+	ofile="${file%$Pattern}md"
 	pandoc -f html -t markdown_github $file -o $ofile 
 	echo "Okay for $ofile"
 done
