97fe33ae127ce427fc87028aeaf8d70ba45da384
mdrights <mdrights@tutanota.de>
2020-02-06 03:20:13 +0000

    add apparmor configs, monitrc, server-opt/user-baseline-2019.sh, et al.

---
+++ server-opt/user-baseline-2019.sh
@@ -0,0 +1,22 @@
+
+echo '#!/bin/sh
+ 
+ /home/linus/repo/tiny-matrix-bot/tiny-matrix-bot.py &
+ 
+ ' > ~/bin/run-csobot.sh
+
+
+cd repo/
+   40  ls
+      41  git clone https://github.com/mdrights/csobot
+	     42  ls -l
+		    43  git clone https://github.com/mdrights/csobot
+			   44  git clone https://github.com/matrix-org/matrix-python-sdk
+			      45  git clone https://github.com/mdrights/tiny-matrix-bot
+				     46  ls -l
+					    47  ls -l ~/bin/
+						   48  cd tiny-matrix-bot/
+						      49  ls
+							     50  cat README.md
+								    51  ln -s ../matrix-python-sdk/matrix_client
+
