8fe9e30d5b840cd01c7f3f85480434474fcc270c
mdrights <mdrights@tutanota.de>
2020-02-07 13:34:28 +0000

    server-opt/user-baseline-2019.sh: updated.

--- server-opt/user-baseline-2019.sh
+++ server-opt/user-baseline-2019.sh
@@ -1,22 +1,29 @@
+#!/bin/sh
+# Use it under a normal user.
 
-echo '#!/bin/sh
+set -eu
+
+mkdir -p ~/bin ~/repo ~/tmp ~/src
+
+
+# Download my bots
+cd ~/repo/
+git clone https://github.com/mdrights/Myscripts
+git clone https://github.com/mdrights/csobot
+git clone https://github.com/mdrights/csobot
+git clone https://github.com/matrix-org/matrix-python-sdk
+git clone https://github.com/mdrights/tiny-matrix-bot
+cd tiny-matrix-bot/
+ln -s ../matrix-python-sdk/matrix_client
+cd -
+
+cp Myscripts/dotfiles/bashrc ~/.bashrc
+
+# Make a startup script for my bot.
+echo "#!/bin/sh
  
- /home/linus/repo/tiny-matrix-bot/tiny-matrix-bot.py &
+ $HOME/repo/tiny-matrix-bot/tiny-matrix-bot.py &
  
- ' > ~/bin/run-csobot.sh
+" > ~/bin/run-csobot.sh
 
-
-cd repo/
-   40  ls
-      41  git clone https://github.com/mdrights/csobot
-	     42  ls -l
-		    43  git clone https://github.com/mdrights/csobot
-			   44  git clone https://github.com/matrix-org/matrix-python-sdk
-			      45  git clone https://github.com/mdrights/tiny-matrix-bot
-				     46  ls -l
-					    47  ls -l ~/bin/
-						   48  cd tiny-matrix-bot/
-						      49  ls
-							     50  cat README.md
-								    51  ln -s ../matrix-python-sdk/matrix_client
-
+exit
