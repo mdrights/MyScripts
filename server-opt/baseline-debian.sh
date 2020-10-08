30f91c03429451d94dfaf39f6508c4ca5d856131
mdrights <mdrights@tutanota.de>
2020-10-08 13:31:43 +0000

    server-opt/baseline-debian.sh: fix --> bind9-dnsutils

--- server-opt/baseline-debian.sh
+++ server-opt/baseline-debian.sh
@@ -49,7 +49,7 @@ apt remove -y gcc locales-all linux-compiler-gcc-8-x86 g++ g++-8 libstdc++-8-dev
 chmod 750 /usr/bin/x86_64-linux-gnu-gcc-* || true
 
 # Use some basic tools.
-apt install -y jq python3-pip python3-requests locales tmux vim curl fail2ban nodejs npm nginx git wget w3m atop htop tmux gpg software-properties-common apt-listbugs apt-listchanges needrestart debsecan debsums libpam-tmpdir libgl1 libxi6 certbot python-certbot-nginx bind9-utils
+apt install -y jq python3-pip python3-requests locales tmux vim curl fail2ban nodejs npm nginx git wget w3m atop htop tmux gpg software-properties-common apt-listbugs apt-listchanges needrestart debsecan debsums libpam-tmpdir libgl1 libxi6 certbot python-certbot-nginx bind9-dnsutils
 
 npm install npm -g
 
